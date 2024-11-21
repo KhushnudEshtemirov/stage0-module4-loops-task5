697a7a2dd25b7f648b4c32f6f8445746d9b1725e
Xushnud Eshtemirov <xushnud122997@gmail.com>
2024-11-21 10:35:16 +0500

    Finished all tasks

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,25 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < 2 * height; j++) {
+                if (i == height - 1 && j == height - 1 || i != height - 1 && (j == i || j == 2 * height - i - 2) || i == 0 && j != 2 * height - 1) {
+                    System.out.print("8");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < 2 * height; j++) {
+                if (i == 0 && j == height - 1 || i != 0 && (j == height - i - 1 || j == height + i - 1) || i == height - 1 && j != 0 && j != 2 * height - 1) {
+                    System.out.print("8");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -2,6 +2,15 @@ package school.mjc.stage0.loops.task5;
 
 public class Triangle {
     public void printTriangle(int cathetusLength) {
-
+        for (int i = 0; i < cathetusLength; i++) {
+            for (int j = 0; j < 2 * cathetusLength; j++) {
+                if (i == 0 && j == cathetusLength - 1 || i != 0 && (j == cathetusLength - i - 1 || j == cathetusLength + i - 1) || i == cathetusLength - 1 && j != 0 && j != 2 * cathetusLength - 1) {
+                    System.out.print("8");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
